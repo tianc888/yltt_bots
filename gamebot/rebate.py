9e581a296e35ff26750ab2345ca98a08b96e1ca9
tianc888 <3826915693@qq.com>
2025-06-24 05:25:17 +0800

    Create rebate.py

---
+++ gamebot/rebate.py
@@ -0,0 +1,5 @@
+async def handle_rebate_log(update, context):
+    await update.message.reply_text("返利日志功能待实现")
+
+async def handle_invite_info(update, context):
+    await update.message.reply_text("邀请信息功能待实现")
