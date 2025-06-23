34457b662b0917d3f9de95d7bd45db15d5cc3f1f
tianc888 <3826915693@qq.com>
2025-06-24 05:25:56 +0800

    Create group.py

---
+++ gamebot/group.py
@@ -0,0 +1,8 @@
+async def handle_kick(update, context):
+    await update.message.reply_text("踢人功能待实现")
+
+async def handle_mute(update, context):
+    await update.message.reply_text("禁言功能待实现")
+
+async def handle_keyword_reply(update, context):
+    pass
