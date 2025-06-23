9a4b9872fafbd2bfc50a9fdd9152d957e4c20af9
tianc888 <3826915693@qq.com>
2025-06-24 05:19:32 +0800

    Create config.py

---
+++ gamebot/config.py
@@ -0,0 +1,5 @@
+TELEGRAM_TOKEN = "你的BotToken"
+GROUP_ID = -100xxxxxxxx    # 游戏群组ID
+ADMINS = [123456789]       # 管理员用户ID或username
+BOT_USERNAME = "你的botusername"
+DATABASE = "wallet.db"     # 与walletbot共用同一db文件
