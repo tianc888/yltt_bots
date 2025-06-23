7765a9af061d8878d12b85e6ba9d878a69265e51
tianc888 <3826915693@qq.com>
2025-06-24 06:41:53 +0800

    Update config.py

--- gamebot/config.py
+++ gamebot/config.py
@@ -1,5 +1,6 @@
-TELEGRAM_TOKEN = "7303119673:AAGAgKHWs760UdtDlVPHIhypYYkiBi_791w"
-GROUP_ID = -1002874860074    # 游戏群组ID
-ADMINS = [8166234652]       # 管理员用户ID或username
-BOT_USERNAME = "Ylttpay.bot"
-DATABASE = "wallet.db"     # 与walletbot共用同一db文件
+TELEGRAM_TOKEN = "你的BotToken"
+ADMINS = [123456789]          # 管理员ID列表
+BOT_USERNAME = "yourbotusername"
+DATABASE = "../wallet.db"     # 与walletbot共用
+RECHARGE_ADDRESS = 'TBt6ASTkRmU9L1qBa3vqr3Jbejif76WEZX'
+RECHARGE_QR_PATH = '../static/recharge_qr.png'
