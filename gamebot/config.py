98b64f5005d5daeeb9dfbdedd80f9cc08ecc608a
tianc888 <3826915693@qq.com>
2025-06-24 03:45:39 +0800

    Update config.py

--- gamebot/config.py
+++ gamebot/config.py
@@ -1,4 +1,4 @@
-TELEGRAM_TOKEN = '你的游戏机器人TOKEN'
+TELEGRAM_TOKEN = '7303119673:AAGAgKHWs760UdtDlVPHIhypYYkiBi_791w'
 BOT_NAME = '渔乐天天游戏'
 RECHARGE_ADDRESS = 'TBt6ASTkRmU9L1qBa3vqr3Jbejif76WEZX'
 RECHARGE_QR_PATH = '../static/recharge_qr.png'  # 注意路径指向 static 目录
