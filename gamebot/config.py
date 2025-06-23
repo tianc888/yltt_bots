7a9064323427749979f869dbf93ac008e68304e0
tianc888 <3826915693@qq.com>
2025-06-24 05:45:28 +0800

    Update config.py

--- gamebot/config.py
+++ gamebot/config.py
@@ -1,5 +1,5 @@
 TELEGRAM_TOKEN = "7303119673:AAGAgKHWs760UdtDlVPHIhypYYkiBi_791w"
 GROUP_ID = -1002874860074    # 游戏群组ID
 ADMINS = [8166234652]       # 管理员用户ID或username
-BOT_USERNAME = "你的botusername"
+BOT_USERNAME = "Ylttpay.bot"
 DATABASE = "wallet.db"     # 与walletbot共用同一db文件
