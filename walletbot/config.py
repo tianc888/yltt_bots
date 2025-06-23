85d0c4d5f07a4fd779c276c26dfe5e502adc901f
tianc888 <3826915693@qq.com>
2025-06-24 03:46:37 +0800

    Update config.py

--- walletbot/config.py
+++ walletbot/config.py
@@ -1,4 +1,4 @@
-TELEGRAM_TOKEN = '你的钱包机器人TOKEN'
+TELEGRAM_TOKEN = '8091927355:AAHg-ySOqOrzUaAE1G4XrWPcc9_tfAcnvWA'
 BOT_NAME = '渔乐天天钱包'
 RECHARGE_ADDRESS = 'TBt6ASTkRmU9L1qBa3vqr3Jbejif76WEZX'
 RECHARGE_QR_PATH = '../static/recharge_qr.png'
