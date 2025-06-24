91b26c9d772a36c8a4f8bcf078b61781624d001a
tianc888 <3826915693@qq.com>
2025-06-24 11:46:39 +0800

    Update config.py

--- gamebot/config.py
+++ gamebot/config.py
@@ -1,7 +1,7 @@
-TELEGRAM_TOKEN = "7303119673:AAGAgKHWs760UdtDlVPHIhypYYkiBi_791w"
-ADMINS = [866234652]          # 管理员ID列表
-BOT_USERNAME = "Ylttk3_bot"
-GROUP_ID = -1002874860074
-DATABASE = "../wallet.db"     # 与walletbot共用
+TELEGRAM_TOKEN = "你的bot token"
+ADMINS = [123456789]  # 管理员tg用户ID
+BOT_USERNAME = "your_bot_username"
+GROUP_ID = -1001234567890
+DATABASE = "wallet.db"
 RECHARGE_ADDRESS = 'TBt6ASTkRmU9L1qBa3vqr3Jbejif76WEZX'
-RECHARGE_QR_PATH = '../static/recharge_qr.png'
+RECHARGE_QR_PATH = 'static/recharge_qr.png'
